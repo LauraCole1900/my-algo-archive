6ff47ba185ec38d6a176645c838e2d51a2ea9c6e
Laura Cole <lauracole1900@comcast.net>
2022-05-24 11:23:38 -0600

    added license, JS folder, and first algo

---
+++ JavaScript/001-arithmetic.js
@@ -0,0 +1,22 @@
+// Fill in the body of each function with the code required
+
+const add = function (num1, num2) {
+  return num1 + num2;
+};
+
+const subtract = function (num1, num2) {
+  return num1 - num2;
+};
+
+const multiply = function (num1, num2) {
+  return num1 * num2;
+};
+
+const divide = function (num1, num2) {
+  return num1 / num2;
+};
+
+console.log(add(8, 4));
+console.log(subtract(8, 4));
+console.log(multiply(8, 4));
+console.log(divide(8, 4));
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # my-algo-archive
+
+This is an archive of algorithms I've come across. They're in no particular order except that in which I encountered them.

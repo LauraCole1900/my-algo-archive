8a9e8b05ffa2f45b5807464c2a44e75caafcf859
Laura Cole <lauracole1900@comcast.net>
2022-05-24 11:27:01 -0600

    updated README; added more algos

---
+++ JavaScript/log-nums.js
@@ -0,0 +1,13 @@
+// Write code to print all numbers from 1 to `num`
+// Assume `num` will be a positive number
+
+const logNums = function (num) {
+  for (let i = 1; i <= num; i++) {
+    console.log(i);
+  }
+};
+
+logNums(8);
+logNums(5);
+logNums(42);
+logNums(15);
---
+++ JavaScript/odd-or-even.js
@@ -0,0 +1,21 @@
+// Write code to return the string "odd" if `num` is an odd number
+// and return the string "even" if `num` is an even number
+
+const oddOrEven = function (num) {
+  if (num % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+};
+
+
+// Alternate using ternary:
+const oddOrEven = function (num) {
+  return num % 2 === 0 ? "even" : "odd";
+};
+
+console.log(oddOrEven(7));
+console.log(oddOrEven(8));
+console.log(oddOrEven(42));
+console.log(oddOrEven(193));

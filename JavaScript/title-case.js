9c39c902637d28823760fe7a18a80a9c8d90bab1
Laura Cole <lauracole1900@comcast.net>
2022-05-26 16:42:19 -0600

    added styleguide version

--- JavaScript/title-case.js
+++ JavaScript/title-case.js
@@ -15,25 +15,45 @@ const titleCase = function (str) {
 
 
 // Alternate using for loop & splitting each word into letters
-const titleCase = function (str) {
-  const result = [];
+// const titleCase = function (str) {
+//   const result = [];
 
-  const words = str.split(" ");
+//   const words = str.split(" ");
 
-  for (let i = 0; i < words.length; i++) {
-    const word = words[i].split("");
+//   for (let i = 0; i < words.length; i++) {
+//     const word = words[i].split("");
 
-    word[0] = word[0].toUpperCase();
+//     word[0] = word[0].toUpperCase();
 
-    result.push(word.join(""));
-  }
+//     result.push(word.join(""));
+//   }
 
-  return result.join(" ");
+//   return result.join(" ");
+// };
+
+
+const titleCaseWithBasicStyleguide = function (str) {
+  const wordArr = str.toLowerCase().split(" ");
+  const nonCapsArr = ["a", "an", "and", "as", "as if", "as long as", "at", "but", "by", "even if", "for", "from", "if", "if only", "in", "into", "like", "near", "now that", "nor", "of", "off", "on", "on top of", "once", "onto", "or", "out of", "over", "past", "so", "so that", "than", "that", "the", "till", "to", "up", "upon", "with", "when", "yet"];
+  const capsArr = wordArr.map((word, i) => {
+    if (i !== 0 && i !== wordArr.length - 1 && (nonCapsArr.includes(word) || nonCapsArr.includes(`${word} ${wordArr[i + 1]} ${wordArr[i + 2]}`) || nonCapsArr.includes(`${wordArr[i - 1]} ${word} ${wordArr[i + 1]}`) || nonCapsArr.includes(`${wordArr[i - 2]} ${wordArr[i - 1]} ${word}`))) {
+      return word;
+    } else {
+      return (word[0].toUpperCase() + word.substring(1))
+    }
+  })
+  return capsArr.join(" ");
 };
 
 
 console.log(titleCase("a christmas carol"));
 console.log(titleCase("the quick brown fox jumped over the lazy dog"));
+
+console.log(titleCaseWithBasicStyleguide("a christmas carol"));
+console.log(titleCaseWithBasicStyleguide("the quick brown fox jumped over the lazy dog"));
+console.log(titleCaseWithBasicStyleguide("As long as I love you I'll be there"));
+console.log(titleCaseWithBasicStyleguide("Stand by even if nothing happens"));
+console.log(titleCaseWithBasicStyleguide("On top of olD smokey"));
 
 
 // List of words not generally capitalized: and, as, as if, as long as, at, but, by, even if, for, from, if, if only, in, into, like, near, now that, nor, of, off, on, on top of, once, onto, or, out of, over, past, so, so that, than, that, till, to, up, upon, with, when, yet

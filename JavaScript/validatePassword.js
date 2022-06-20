76ec321e11a32a90bba9448c9dfea8049c2d91b1
Laura Cole <lauracole1900@comcast.net>
2022-06-19 23:08:03 -0600

    added validatePassword algo

---
+++ JavaScript/validatePassword.js
@@ -0,0 +1,43 @@
+// Write a function that takes a string as a parameter and determines if it is a valid password with the following constraints:
+
+// 1. It must be at least 8 characters long
+// 2. It must contain at least 1 capital letter
+// 3. It must contain at least 1 lower case letter
+
+// Output true if the string is a valid password or false otherwise.
+
+// Ex:
+// Input: RexTheDog
+// Output: true
+
+// Input: rexthedog
+// Output: false
+
+// Input: REXTHEDOG
+// Output: false
+
+// Input: Dog
+// Output: false
+
+
+
+// Official solution:
+var isValidPass = function (password) {
+  var hasLower = false;
+  var hasUpper = false;
+
+  if (password.length < 8) {
+    return false;
+  }
+
+  for (var i = 0; i < password.length; i++) {
+    if (password[i].toLowerCase() === password[i]) {
+      hasLower = true;
+    }
+    if (password[i].toUpperCase() === password[i]) {
+      hasUpper = true;
+    }
+  }
+
+  return hasLower && hasUpper;
+};

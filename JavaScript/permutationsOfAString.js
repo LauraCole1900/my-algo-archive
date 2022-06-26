de5398fdd9ee0d97ee77058e61ec4aff5e50e23d
Laura Cole <lauracole1900@comcast.net>
2022-06-25 23:59:21 -0600

    added to permutationsOfAString; added peakFinder

--- JavaScript/permutationsOfAString.js
+++ JavaScript/permutationsOfAString.js
@@ -2,14 +2,16 @@
 
 // Write code to create a function that accepts a string and returns all permutations of that string
 
-const permutationString = function (str) {
-  // Takes in a string
-  // Splits string into array?
-  // Iterates over array -> nested for loop?
-  // Re-joins letters in new order
-  // Checks whether given solution is already in array -- what about duplicate letters?
-  // Pushes solution to array
 
+// Takes in a string
+// Splits string into array?
+// Iterates over array -> nested for loop?
+// Re-joins letters in new order
+// Checks whether given solution is already in array -- what about duplicate letters?
+// Pushes solution to array
+
+
+const permutationString = function (str) {
   if (!str || typeof str !== "string") {
     return "Please enter a string"
   } else if (str.length === 1) {

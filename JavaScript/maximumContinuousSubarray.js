5d437350d0cffec17cfee2429191ea4bf49ae8e3
Laura Cole <lauracole1900@comcast.net>
2022-07-01 10:43:21 -0600

    added starter for maximumContinuousSubarray algo

---
+++ JavaScript/maximumContinuousSubarray.js
@@ -0,0 +1,5 @@
+// Create a function that takes in an array and returns the sum of the maximum continuous, or contiguous, subarray
+
+const maxSumArray = function (numbers) {
+  // Write your solution here
+};

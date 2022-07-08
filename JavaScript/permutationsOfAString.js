9eb038467877bc8d90147fe5806a26d8c7496ba8
Laura Cole <lauracole1900@comcast.net>
2022-07-08 05:07:57 -0600

    added solution to permutationsOfAString algo

--- JavaScript/permutationsOfAString.js
+++ JavaScript/permutationsOfAString.js
@@ -11,6 +11,28 @@
 // Pushes solution to array
 
 
+// Keeps duplicates
+const permutationString = function (str) {
+  if (!str || typeof str !== "string") {
+    return "Please enter a string"
+  } else if (str.length === 1) {
+    return str;
+  }
+
+  const strArr = str.split("")
+  let resultsArray = [];
+
+  strArr.map((letter, i) => {
+    let remainingLetters = str.slice(0, i) + str.slice(i + 1, str.length);
+    for (let permut of permutationString(remainingLetters)) {
+      resultsArray.push(letter + permut)
+    }
+  })
+  return resultsArray;
+};
+
+
+// Removes duplicates
 const permutationString = function (str) {
   if (!str || typeof str !== "string") {
     return "Please enter a string"
@@ -20,9 +42,16 @@ const permutationString = function (str) {
 
   let resultsArray = [];
 
-  str.map((letter, i) => {
-
-  })
+  for (let i = 0; i < str.length; i++) {
+    const letter = str[i]
+    if (str.indexOf(letter) !== i) {
+      continue
+    }
+    let remainingLetters = str.slice(0, i) + str.slice(i + 1, str.length);
+    for (let permut of permutationString(remainingLetters)) {
+      resultsArray.push(letter + permut)
+    }
+  }
   return resultsArray;
 };
 
@@ -30,6 +59,6 @@ const permutationString = function (str) {
 
 // };
 
-// const permutationString = function (str) {
-
-// };
+console.log(permutationString("dog"));
+console.log(permutationString("sing"));
+console.log(permutationString("bobby"));

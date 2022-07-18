02119f1fe80d32b785f18f926315544ebc661011
Laura Cole <lauracole1900@comcast.net>
2022-07-18 15:01:50 -0600

    added a solution for minIncrementForUnique algo

--- JavaScript/minIncrementForUnique.js
+++ JavaScript/minIncrementForUnique.js
@@ -5,3 +5,21 @@ const minIncrement = function (nums) {
   // single digits only?
 
 };
+
+
+// Official solution
+const minIncrement = function (nums) {
+  let uniqueNums = {};
+  let increments = 0;
+
+  for (var i = 0; i < nums.length; i++) {
+    while (uniqueNums[nums[i]] === true) {
+      nums[i] += 1;
+      increments++;
+    }
+
+    uniqueNums[nums[i]] = true;
+  }
+
+  return increments;
+};
